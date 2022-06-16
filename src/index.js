96154c9348bb3a32f94d6c74626c421bd52c1b47
skan75 <negachyov@gmail.com>
2022-06-16 19:07:33 +0300

    Настроил WebPack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import Card from './Card.js';
-import FormValidator from "./FormValidator.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import './pages/index.css';
+import Card from './scripts/Card.js';
+import FormValidator from "./scripts/FormValidator.js";
+import PopupWithImage from "./scripts/PopupWithImage.js";
+import PopupWithForm from "./scripts/PopupWithForm.js";
+import UserInfo from "./scripts/UserInfo.js";
 
 const btnProfile = document.querySelector("#button__profile");
 const profileForm = document.querySelector("#profile");
